fc8d191786fad80e1e4a9e71ed1f06ed49c9753e
mathias <mathias@mathias-desktop.(none)>
2010-01-02 16:29:42 +0100

    Added license file.

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from core import *
 
 from twisted.internet import reactor
 
-factory = VTKBotFactory(nickname="VTKBot", server="vtk.ugent.be", channels=["#trivia"])
+factory = VTKBotFactory(nickname="VTKBot", server="vtk.ugent.be", channels=["#vtk"])
 reactor.connectTCP("vtk.ugent.be", 6667, factory)
 reactor.run()
